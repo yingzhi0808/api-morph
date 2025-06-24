7f38b84e2e8ad9df06aff2df805b2c0cc1ba2744
纪颖志 <jiyingzhi0808@gmail.com>
2025-06-24 14:02:10 +0800

    Fix: the middlewares has not been exported

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export * from "./analyzers";
 export * from "./builders";
 export * from "./core";
 export * from "./lib";
+export * from "./middlewares";
 export * from "./parsers";
 export * from "./types";
